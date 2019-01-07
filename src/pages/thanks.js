78d39db090fbfde9552abd0eb396b5ec9d411fa2
Romain Lebascle <romain.lebascle@gmail.com>
2019-01-07 15:42:36 +0100

    style hanks page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -3,8 +3,24 @@ import Layout from "../components/layout";
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>MERCI</h1>
-    <p>Je reviendrais vers vous dans les plus brefs délais.</p>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh"
+      }}
+    >
+      <h1
+        style={{
+          color: "hsla(83, 60%, 45%, 1)"
+        }}
+      >
+        MERCI
+      </h1>
+      <p>Je reviendrais vers vous dans les plus brefs délais.</p>
+    </div>
   </Layout>
 );
 
