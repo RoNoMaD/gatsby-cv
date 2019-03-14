c3874416d942f7ae08830f397330cc9669bd6b56
Romain Lebascle <romain.lebascle@gmail.com>
2019-03-14 13:40:50 +0100

    feat: use hook for static query

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,45 +1,43 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
-import { StaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 
 import "./layout.css";
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
+const Layout = ({ children }) => {
+  const { site } = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
         }
       }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            {
-              name: "description",
-              content:
-                "Romain Lebascle Développeur Web Full Stack Curiculum Vitae"
-            },
-            {
-              name: "keywords",
-              content:
-                "romain, lebascle, développeur, web, front-end, front, react, javascript, css, graphql, gatsby"
-            }
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <div>{children}</div>
-      </>
-    )}
-  />
-);
+    }
+  `);
+  return (
+    <>
+      <Helmet
+        title={site.siteMetadata.title}
+        meta={[
+          {
+            name: "description",
+            content:
+              "Romain Lebascle Développeur Web Full Stack Curiculum Vitae"
+          },
+          {
+            name: "keywords",
+            content:
+              "romain, lebascle, développeur, web, front-end, front, react, javascript, css, graphql, gatsby"
+          }
+        ]}
+      >
+        <html lang="fr" />
+      </Helmet>
+      <div>{children}</div>
+    </>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired
