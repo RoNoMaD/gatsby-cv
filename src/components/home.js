0e4497a82faf5c4f4ee96fd9be0e77b2d1876e03
Romain Lebascle <romain.lebascle@gmail.com>
2019-04-13 10:25:09 +0200

    feat: add linaria on home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,53 +1,81 @@
 import React from "react";
+import { css } from "linaria";
 
 import QuoteIcon from "../images/iconmonstr-quote-3.svg";
 
-import "./home.css";
+const homeSection = css`
+  height: 100vh;
+  /* padding: "0 10px",*/
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0px 1.0875rem 1.45rem;
+  background: linear-gradient(
+    135deg,
+    rgba(33, 33, 33, 1) 0%,
+    rgba(48, 48, 48, 1) 35%,
+    rgba(66, 66, 66, 1) 100%
+  );
+`;
+
+const homeAnimatedName = css`
+  color: hsla(83, 60%, 45%, 1);
+  overflow: hidden; /* Ensures the content is not revealed until the animation */
+  border-right: 0.15em solid hsla(83, 60%, 45%, 1); /* The typwriter cursor */
+  white-pace: nowrap; /* Keeps the content on a single line */
+  margin: 0 auto 1.45rem auto; /* Gives that scrolling effect as the typing happens */
+  animation: typing 2.5s steps(30, end), blink-caret 0.5s step-end infinite;
+
+  /* The typing effect */
+  @keyframes typing {
+    from {
+      width: 0;
+    }
+    to {
+      width: 100%;
+    }
+  }
+
+  /* The typewriter cursor effect */
+  @keyframes blink-caret {
+    from,
+    to {
+      border-color: transparent;
+    }
+    50% {
+      border-color: hsla(83, 60%, 45%, 1);
+    }
+  }
+`;
+
+const homeSubtitle = css`
+  textalign: center;
+`;
+
+const quoteContainer = css`
+  display: flex;
+`;
+
+const quoteIcon = css`
+  width: 50px;
+  fill: hsla(83, 60%, 45%, 1);
+`;
+
+const quoteText = css`
+  padding: 8px;
+`;
 
 const Home = () => (
-  <section
-    style={{
-      height: "100vh",
-      // padding: "0 10px",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      justifyContent: "center",
-      padding: "0px 1.0875rem 1.45rem",
-      background:
-        "linear-gradient(135deg, rgba(33,33,33,1) 0%, rgba(48,48,48,1) 35%, rgba(66,66,66,1) 100%)"
-    }}
-  >
+  <section className={homeSection}>
     <h2>Bonjour je suis </h2>
     <div>
-      <h1
-        style={{
-          color: "hsla(83, 60%, 45%, 1)",
-          overflow:
-            "hidden" /* Ensures the content is not revealed until the animation */,
-          borderRight:
-            ".15em solid hsla(83, 60%, 45%, 1)" /* The typwriter cursor */,
-          whiteSpace: "nowrap" /* Keeps the content on a single line */,
-          margin:
-            "0 auto 1.45rem auto" /* Gives that scrolling effect as the typing happens */,
-          // letterSpacing: ".15em" /* Adjust as needed */,
-          animation:
-            "typing 2.5s steps(30, end),    blink-caret .5s step-end infinite"
-        }}
-      >
-        {"<Romain Lebascle />"}
-      </h1>
+      <h1 className={homeAnimatedName}>{"<Romain Lebascle />"}</h1>
     </div>
-    <h2 style={{ textAlign: "center" }}>
-      Développeur Web Full Stack basé à Nantes.
-    </h2>
-    <div style={{ display: "flex" }}>
-      <QuoteIcon style={{ width: "50px", fill: "hsla(83, 60%, 45%, 1)" }} />
-      <p
-        style={{
-          padding: "8px"
-        }}
-      >
+    <h2 className={homeSubtitle}>Développeur Web Full Stack basé à Nantes.</h2>
+    <div className={quoteContainer}>
+      <QuoteIcon className={quoteIcon} />
+      <p className={quoteText}>
         {
           "Avec 5 ans d'expérience dans le développement de sites et d'applications web et mobile et une passion pour l'écosystème Front End."
         }
