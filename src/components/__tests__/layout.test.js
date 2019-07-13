7e99e6f946bea78931b0453cee63a634ead28835
Romain Lebascle <romain.lebascle@gmail.com>
2019-07-13 11:06:48 +0200

    chore: upgrade dependencies

--- src/components/__tests__/layout.test.js
+++ src/components/__tests__/layout.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "react-testing-library";
+import { render } from "@testing-library/react";
 import { useStaticQuery } from "gatsby"; // mocked
 
 import Layout from "../layout";
