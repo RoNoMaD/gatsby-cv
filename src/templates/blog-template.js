98c5b5b4befed7287e335b3e522ab74c707cea40
Romain Lebascle <romain.lebascle@gmail.com>
2020-03-02 15:10:37 +0100

    chore: upgrade storybook and addons

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,5 +1,43 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { css } from "linaria";
+
+import Layout from "../components/layout";
+
+const article = css`
+  max-width: var(--bp-med);
+  margin-right: auto;
+  margin-left: auto;
+  padding: var(--spacing-large);
+
+  & a {
+    color: var(--color-neutral-white);
+    text-decoration: underline;
+
+    &:hover {
+      color: var(--color-brand-green);
+    }
+
+    &:active {
+      color: var(--color-brand-green);
+    }
+  }
+`;
+
+const title = css`
+  color: var(--color-brand-green);
+`;
+
+const date = css`
+  font-size: var(--font-size-sm);
+  font-style: italic;
+`;
+
+const content = css`
+  & > * + * {
+    padding-top: var(--spacing);
+  }
+`;
 
 export default function Template({
   data // this prop will be injected by the GraphQL query below.
@@ -7,16 +45,13 @@ export default function Template({
   const { markdownRemark } = data; // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark;
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
+    <Layout>
+      <article className={article}>
+        <h1 className={title}>{frontmatter.title}</h1>
+        <div className={date}>{frontmatter.date}</div>
+        <div className={content} dangerouslySetInnerHTML={{ __html: html }} />
+      </article>
+    </Layout>
   );
 }
 export const pageQuery = graphql`
