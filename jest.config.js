adf8bd570f6eba7162ff0a91f3a37fb404ae6aa8
Romain Lebascle <romain.lebascle@gmail.com>
2020-03-23 17:00:37 +0100

    test: jest transpile lodash-es

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,26 @@
 module.exports = {
   // js or jsx files need to be transformed using jest-preprocess.js file
   transform: {
-    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`
+    "^.+\\.jsx?$": `<rootDir>/jest-preprocess.js`,
   },
   moduleNameMapper: {
     // mocking stylesheets with identity-obj-proxy
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     // mocking other assets with custom mock
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`
+    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/__mocks__/file-mock.js`,
   },
   testPathIgnorePatterns: [
     `node_modules`,
     `.cache`,
     `.linaria-cache`,
-    `public`
+    `public`,
   ],
   modulePathIgnorePatterns: ["<rootDir>/node_modules"],
   watchPathIgnorePatterns: ["<rootDir>/node_modules/"],
   // tell Jest to transpile Gatsby files
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+  transformIgnorePatterns: [`node_modules/(?!(gatsby)|lodash-es/)`],
   globals: {
-    __PATH_PREFIX__: ``
+    __PATH_PREFIX__: ``,
   },
   setupFiles: [`<rootDir>/loadershim.js`],
   setupFilesAfterEnv: ["<rootDir>/setup-test-env.js"],
@@ -28,6 +28,6 @@ module.exports = {
   // watch plugin to filter on file name
   watchPlugins: [
     "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname"
-  ]
+    "jest-watch-typeahead/testname",
+  ],
 };
