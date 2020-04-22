dde1f402d35dc316cc3e730a87f5a78b78723234
Romain Lebascle <romain.lebascle@gmail.com>
2020-04-22 14:32:08 +0200

    chore: upgrade libs

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { css } from "linaria";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 import Header from "./header";
 import Footer from "./footer/footer";
